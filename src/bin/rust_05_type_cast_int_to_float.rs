1410d7f7d39a87f45a74d224f80eb4f3ddd93746
EIBerlin <>
2026-04-13 15:02:32 +0900

    rust: type cast int to flt

---
+++ src/bin/rust_05_type_cast_int_to_float.rs
@@ -0,0 +1,16 @@
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
+pub fn main() {
+    let my_int: i8 = 100;
+    println!(
+        "before val is :: {} , type is :: {}",
+        my_int,
+        type_of(my_int)
+    );
+    let my_float = my_int as f32;
+    println!("val is :: {} , type is :: {}", my_float, type_of(my_float));
+}
