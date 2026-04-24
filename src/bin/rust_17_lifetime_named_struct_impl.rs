cbc5ba7d4cd8be67de28ea55d43ecca16a051485
EIBerlin <>
2026-04-24 19:18:14 +0900

    rust: lifetime named struct

---
+++ src/bin/rust_17_lifetime_named_struct_impl.rs
@@ -0,0 +1,16 @@
+struct MyStruct<'l> {
+    x: &'l str,
+}
+
+impl<'l> MyStruct<'l> {
+    fn lifetime_generic(&self, _a: &str, _b: &str) -> &str {
+        self.x
+    }
+}
+
+pub fn main() {
+    let a = "a";
+    let b = "a";
+    let my_struct = MyStruct { x: &a };
+    my_struct.lifetime_generic(a, b);
+}
