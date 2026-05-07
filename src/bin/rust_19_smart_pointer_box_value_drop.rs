9a0d29949c57cc2947f61b779fcd732d1330c26f
EIBerlin <>
2026-05-07 19:51:20 +0900

    rust: deref smart pointer

---
+++ src/bin/rust_19_smart_pointer_box_value_drop.rs
@@ -0,0 +1,31 @@
+use std::ops::Deref;
+
+struct MBox<T>(T);
+
+impl<T> MBox<T> {
+    fn new(x: T) -> MBox<T> {
+        MBox(x)
+    }
+}
+
+impl<T> Deref for MBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> Drop for MBox<T> {
+    fn drop(&mut self) {
+        println!("value dropped!")
+    }
+}
+
+pub fn main() {
+    let x = 5;
+    {
+        let y = MBox::new(x);
+        println!("value created! y is {:?}", *y);
+    }
+    println!("end scope!")
+}
